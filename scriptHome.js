253d0e717237b68fcb0cbf66134f57e7f64a51b0
TheAmazingAmatuer <81491206+TheAmazingAmatuer@users.noreply.github.com>
2021-03-27 19:03:35 -0400

    Add files via upload
    Home JavaScript

---
+++ scriptHome.js
@@ -0,0 +1,15 @@
+function stopMotionPage(){
+    window.location.assign("");
+}
+
+function td1Page(){
+    window.location.assign("file:///C:/Users/Hp-Omen/Documents/Coding/Classes/Home/td1/td1Main.html");
+}
+
+function td2Page(){
+    window.location.assign("");
+}
+
+function td3MotionPage(){
+    window.location.assign("");
+}
