7e80fbba655c85d6ae542c7678e221ed5cd40378
TheAmazingAmatuer <81491206+TheAmazingAmatuer@users.noreply.github.com>
2021-03-27 20:36:16 -0400

    Update scriptHome.js

--- scriptHome.js
+++ scriptHome.js
@@ -3,7 +3,7 @@ function stopMotionPage(){
 }
 
 function td1Page(){
-    window.location.assign("file:///C:/Users/Hp-Omen/Documents/Coding/Classes/Home/td1/td1Main.html");
+    window.location.assign("td1Main.html");
 }
 
 function td2Page(){
