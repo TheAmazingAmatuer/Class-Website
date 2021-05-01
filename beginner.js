c229af2f2dda0d1f869a743cdf10dd26e3a104f3
TheAmazingAmatuer <81491206+TheAmazingAmatuer@users.noreply.github.com>
2021-05-01 19:51:00 -0400

    Update beginner.js

--- beginner.js
+++ beginner.js
@@ -8,9 +8,7 @@ var client = new XMLHttpRequest();
                 document.getElementById("h1").innerHTML = txt[0];
                 document.getElementById("txt1").innerHTML = txt[1];
                 document.getElementById("h2").innerHTML = txt[2];
-                document.getElementById("txt2").innerHTML = txt[3];
-                document.getElementById("h3").innerHTML = txt[4];
-                document.getElementById("txt3").innerHTML = txt[5];
+               
             }
         }
         client.send();
